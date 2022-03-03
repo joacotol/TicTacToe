c6f9d0e88f6d68d8bbd227035b0a634072163fd8
joacotol <joacochrist@gmail.com>
2022-03-02 20:17:55 -0500

    Added an object variable of CharacterList class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        CharacterList list = new CharacterList();
         Scanner scanner = new Scanner(System.in);
-        UserInterface ui = new UserInterface(scanner);
+        UserInterface ui = new UserInterface(scanner, list);
         ui.start();
     }
 }
