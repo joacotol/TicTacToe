dccf63e9c0364c73cc4ad99f8a689df3e3e3c85d
joacotol <joacochrist@gmail.com>
2022-03-05 15:24:06 -0500

    Implemented new methods that involves Objective 3

--- src/main/CharacterList.java
+++ src/main/CharacterList.java
@@ -1,16 +1,24 @@
 package main;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CharacterList {
-    ArrayList<Character> charList;
+    private ArrayList<Character> charList;
+    private boolean xWins;
+    private boolean oWins;
+    final private int TOTALX = 264;
+    final private int TOTALO = 237;
 
     public CharacterList() {
         this.charList = new ArrayList<>();
+        this.xWins = false;
+        this.oWins = false;
     }
 
+    // Objective 2
     public void addCharacters(String symbols) {
-        for (int i = 0;i < symbols.length();i++) {
+        for (int i = 0; i < symbols.length(); i++) {
             char symbol = symbols.charAt(i);
             charList.add(symbol);
         }
@@ -19,7 +27,7 @@ public class CharacterList {
     public void printBoard() {
         System.out.println("---------");
         System.out.print("| ");
-        for (int j = 0; j <= charList.size();j++) {
+        for (int j = 0; j <= charList.size(); j++) {
             if (j == 3 || j == 6) {
                 System.out.print("|\n| ");
             }
@@ -33,5 +41,96 @@ public class CharacterList {
         System.out.println("---------");
     }
 
+    // Objective 3
+    // All methods below are for Objective 3
 
+    public void checkHorizontal() {
+        for (int i = 0; i < charList.size(); i += 3) {
+            if (charList.get(i) + charList.get(i + 1) + charList.get(i + 2) == TOTALX) {
+                xWins = true;
+            } else if (charList.get(i) + charList.get(i + 1) + charList.get(i + 2) == TOTALO) {
+                oWins = true;
+            }
+        }
+    }
+
+    public void checkVertical() {
+        for (int i = 0; i < 3; i++) {
+            if (charList.get(i) + charList.get(i + 3) + charList.get(i + 6) == TOTALX) {
+                xWins = true;
+            } else if (charList.get(i) + charList.get(i + 3) + charList.get(i + 6) == TOTALO) {
+                oWins = true;
+            }
+        }
+    }
+
+    public void checkDiagonal() {
+        for (int i = 0; i < 3; i += 2) {
+            // Left Diagonal
+            if (charList.get(i) + charList.get(i + 4) + charList.get(charList.size() - 1) == TOTALX) {
+                xWins = true;
+            } else if (charList.get(i) + charList.get(i + 4) + charList.get(charList.size() - 1) == TOTALO) {
+                oWins = true;
+            }
+
+            // Right Diagonal
+            if (charList.get(i) + charList.get(i + 2) + charList.get(i + 4) == TOTALX) {
+                xWins = true;
+            } else if (charList.get(i) + charList.get(i + 2) + charList.get(i + 4) == TOTALO) {
+                oWins = true;
+            }
+        }
+    }
+
+    public void checkTheWinner() {
+        checkHorizontal();
+        checkVertical();
+        checkDiagonal();
+        if (xWins && !oWins) {
+            System.out.println("X Wins");
+            return;
+        } else if (!xWins && oWins) {
+            System.out.println("O Wins");
+            return;
+        }
+
+        if (!xWins && !oWins && !hasEmptyCells()) {
+            System.out.println("Draw");
+        } else if ((xWins && oWins) || isImpossible()){
+            System.out.println("Impossible");
+        } else {
+            System.out.println("Game not finished");
+        }
+
+    }
+
+    public boolean isImpossible() {
+        int xCount = 0;
+        int oCount = 0;
+
+        for (Character single : charList) {
+            if (single.equals('X')) {
+                xCount++;
+            } else if (single.equals('O')) {
+                oCount++;
+            }
+        }
+
+        if (xCount - oCount > 1 || oCount - xCount > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Game not Finished
+    // when neither side has three a row but the grid still has empty cells
+    public boolean hasEmptyCells() {
+        for (Character single : charList) {
+            if (single.equals('_')) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
